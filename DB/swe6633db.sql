424e45a996cae1478f8f2d30f5822e2c88267cb8
amritrjakacabu015 <amritrjakacabu015@gmail.com>
2017-11-24 19:34:56 -0500

    made adjustments to teamMembers Table

--- DB/swe6633db.sql
+++ DB/swe6633db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 12, 2017 at 05:21 AM
+-- Generation Time: Nov 25, 2017 at 12:33 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -31,6 +31,7 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `functionalrequirements`;
 CREATE TABLE IF NOT EXISTS `functionalrequirements` (
   `projectID` bigint(10) NOT NULL,
+  `fReqID` bigint(10) NOT NULL,
   `fRequirement` varchar(200) NOT NULL,
   `fReqAnalysisHours` int(5) NOT NULL,
   `fReqDesignHours` int(5) NOT NULL,
@@ -48,7 +49,8 @@ CREATE TABLE IF NOT EXISTS `functionalrequirements` (
 DROP TABLE IF EXISTS `nonfunctionalrequirements`;
 CREATE TABLE IF NOT EXISTS `nonfunctionalrequirements` (
   `projectID` bigint(10) NOT NULL,
-  `nonFunctionalRequirements` int(11) NOT NULL,
+  `nFReqID` bigint(10) NOT NULL,
+  `nonFunctionalRequirements` varchar(200) NOT NULL,
   `nFRAnalysisHours` int(5) NOT NULL,
   `nFRDesignHours` int(5) NOT NULL,
   `nFRDevelopmentHours` int(5) NOT NULL,
@@ -67,7 +69,9 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `projectID` bigint(10) NOT NULL,
   `projectName` varchar(100) NOT NULL,
   `projectOwner` varchar(100) NOT NULL,
-  `projectTeam` varchar(1000) NOT NULL,
+  `projectTeam` varchar(10000) NOT NULL,
+  `startDate` varchar(11) NOT NULL,
+  `endDate` varchar(11) NOT NULL,
   PRIMARY KEY (`projectID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -80,20 +84,16 @@ CREATE TABLE IF NOT EXISTS `projects` (
 DROP TABLE IF EXISTS `projectteam`;
 CREATE TABLE IF NOT EXISTS `projectteam` (
   `projectID` bigint(10) NOT NULL,
+  `memberID` bigint(10) NOT NULL,
+  `teamMemberName` varchar(100) NOT NULL,
   `designation` varchar(30) NOT NULL,
   `analysisHours` int(5) NOT NULL,
   `designHours` int(5) NOT NULL,
   `developmentHours` int(5) NOT NULL,
   `testingHours` int(5) NOT NULL,
-  `managementHours` int(5) NOT NULL
+  `managementHours` int(5) NOT NULL,
+  PRIMARY KEY (`memberID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS 'projectteamperson';
-CREATE TABLE IF NOT EXISTS 'projectteamperson' (
-	'projectID' bigint(10) not NULL,
-	'idperson' INT not null
-)
 
 -- --------------------------------------------------------
 
@@ -110,19 +110,8 @@ CREATE TABLE IF NOT EXISTS `projecttools` (
   `testingTools` varchar(50) NOT NULL,
   `managementTools` varchar(50) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-DROP TABLE IF EXISTS 'person'
-CREATE TABLE `person` (
-  `idperson` INT NOT NULL,
-  `first_name` VARCHAR(40) NULL,
-  `last_name` VARCHAR(40) NULL,
-  `title` VARCHAR(40) NULL,
-  PRIMARY KEY (`idperson`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
-COMMENT = 'Person information Table';
-COMMIT;
