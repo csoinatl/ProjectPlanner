cc1e354950dcf9ac2b1ca8d206f476c9a3d42306
Charlie So <cso@revenueanalytics.com>
2017-12-03 18:36:20 -0500

    Adding files for test

--- API/routes.py
+++ API/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from models import Project, Member
+from models import Project
 from DBUtil import db
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import exists
 
 import json
 
@@ -42,3 +44,26 @@ def projectlist():
     :return: Detailed information for a single project
     """
     return render_template('projectdetail.html')
+
+
+if __name__ == '__main__':
+    connection = db.engine.connect()
+    Session = sessionmaker(bind=db.engine)
+    session = Session()
+
+    # run some stuff here:
+    # Insert a project
+
+    proj = Project
+    proj.projectID = '1'
+    proj.projectName = 'Test project'
+    proj.projectTeam = 'Charlie So'
+
+    session.add(proj)
+    session.commit()
+
+    if session.query(exists().where(Project.projectName == 'Test project')).scalar():
+        res = session.query(Project).filter_by(projectName='Test project').first()
+        print res
+
+    connection.close()
