f2139a99545c1052e5a58f044c6845548a2b1132
amritrjakacabu015 <amritrjakacabu015@gmail.com>
2017-12-03 20:33:41 -0500

    verify?

--- API/models.py
+++ API/models.py
@@ -9,44 +9,45 @@ class Project(db.Model):
     """
 
     __tablename__ = 'projects'
-    projectID = db.Column(db.BigInteger, primary_key=True)
+    projectID = db.Column(db.BigInteger(10), primary_key=True)
     projectName = db.Column(db.String(100))
+	projectDesc = db.Column(db.String(1000))
     projectOwner = db.Column(db.BigInteger)
-    projectTeam = db.Column(db.Integer)
+    projectTeam = db.Column(db.String(1000))
+	startDate = db.Colum(db.String(11))
+	endDate = db.Column(db.String(11))
 
     def __init__(self, project_id, project_name, project_owner, project_team):
-        self.projectID = project_id
+		self.projectID = project_id
         self.projectName = project_name
+		self.projectDesc = project_desc
         self.projectOwner = project_owner
         self.projectTeam = project_team
+		self.startDate = start_date
+		self.endDate = end_date
 
 
-# class Requirements(db.Model):
+class Requirements(db.Model):
 #     """
-#     Model for functional requirements from DB
+#     Model for requirements from DB
 #     """
-#
-#     __tablename__ = 'requirements'
-#     projectID = db.Column(db.BigInteger)
-#     fRequirement = db.Column(db.String(200))
-#     fReqAnalysisHours = db.Column(db.Integer)
-#     fReqDesignHours = db.Column(db.Integer)
-#     fReqDevelopmentHours = db.Column(db.Integer)
-#     fReqTestingHours = db.Column(db.Integer)
-#     fReqManagementHours = db.Column(db.Integer)
-#
-#     def __init__(self, project_id, func_req, func_req_anal_hrs, func_rec_des_hrs,
-#                  func_rec_dev_hrs, func_req_test_hrs, func_req_mngt_hrs):
-#         self.projectID = project_id
-#         self.fRequirement = func_req
-#         self.fReqAnalysisHours = func_req_anal_hrs
-#         self.fReqDesignHours = func_rec_des_hrs
-#         self.fReqDevelopmentHours = func_rec_dev_hrs
-#         self.fReqTestingHours = func_req_test_hrs
-#         self.fReqManagementHours = func_req_mngt_hrs
 
+    __tablename__ = 'requirements'
+    projectID = db.Column(db.BigInteger(10))
+	reqID = db.Ccolumn(db.BigInteger(10))
+    requirements = db.Column(db.String(200))
+	reqType = db.Column(db.Integer(1))
+    
 
-# class ProjectTeam(db.Model):
+    def __init__(self, project_id, func_req, func_req_anal_hrs, func_rec_des_hrs,
+        func_rec_dev_hrs, func_req_test_hrs, func_req_mngt_hrs):
+        self.projectID = project_id
+		self.reqIDd = req_id
+        self.requirements = func_req
+        self.reqType = req_type
+   
+
+class ProjectTeam(db.Model):
 #     """
 #     Model for project teams
 #     """
