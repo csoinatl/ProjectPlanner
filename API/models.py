76fee73b50af70bec15602cb963dc1770834e064
Charlie So <cso@revenueanalytics.com>
2017-12-03 21:02:55 -0500

    Adding Emberjs code, finally.

--- API/models.py
+++ API/models.py
@@ -11,20 +11,20 @@ class Project(db.Model):
     __tablename__ = 'projects'
     projectID = db.Column(db.BigInteger(10), primary_key=True)
     projectName = db.Column(db.String(100))
-	projectDesc = db.Column(db.String(1000))
+    projectDesc = db.Column(db.String(1000))
     projectOwner = db.Column(db.BigInteger)
     projectTeam = db.Column(db.String(1000))
-	startDate = db.Colum(db.String(11))
-	endDate = db.Column(db.String(11))
+    startDate = db.Column(db.String(11))
+    endDate = db.Column(db.String(11))
 
-    def __init__(self, project_id, project_name, project_owner, project_team):
-		self.projectID = project_id
+    def __init__(self, project_id, project_name, project_desc, project_owner, project_team, start_date, end_date):
+        self.projectID = project_id
         self.projectName = project_name
-		self.projectDesc = project_desc
+        self.projectDesc = project_desc
         self.projectOwner = project_owner
         self.projectTeam = project_team
-		self.startDate = start_date
-		self.endDate = end_date
+        self.startDate = start_date
+        self.endDate = end_date
 
 
 class Requirements(db.Model):
@@ -34,15 +34,14 @@ class Requirements(db.Model):
 
     __tablename__ = 'requirements'
     projectID = db.Column(db.BigInteger(10))
-	reqID = db.Ccolumn(db.BigInteger(10))
+    reqID = db.Ccolumn(db.BigInteger(10))
     requirements = db.Column(db.String(200))
-	reqType = db.Column(db.Integer(1))
+    reqType = db.Column(db.Integer(1))
     
 
-    def __init__(self, project_id, func_req, func_req_anal_hrs, func_rec_des_hrs,
-        func_rec_dev_hrs, func_req_test_hrs, func_req_mngt_hrs):
+    def __init__(self, project_id, req_id, func_req, req_type):
         self.projectID = project_id
-		self.reqIDd = req_id
+        self.reqIDd = req_id
         self.requirements = func_req
         self.reqType = req_type
    
---
+++ Planner/app/router.js
@@ -0,0 +1,18 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('about');
+  this.route('logsheet');
+  this.route('planform');
+  this.route('tutorials');
+  this.route('home');
+  this.route('contact');
+});
+
+export default Router;
