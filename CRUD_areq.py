a364b18a89a223af48704898513d4b702e7357c7
JohnMcKinney6 <33961934+JohnMcKinney6@users.noreply.github.com>
2017-12-03 21:45:45 -0600

    Add files via upload
    Added delete functions and add functions.  There may be an error in the add functions which I can't seem to resolve.

--- CRUD_areq.py
+++ CRUD_areq.py
@@ -4,24 +4,86 @@ sqlCursor = conn.cursor()
 
 
 ##------------------------
-## Functional Requirements
-##------------------------
 def addrequirements(pid, rid, reqtext, rtype):
      "Add a new requirement to the requirements table"
-     add_req = ("INSERT INTO reqirements "
-           "(projectID, reqID, requirements, reqType"
+     add_req = ("INSERT INTO requirements "
+           "(projectID, reqID, requirements, reqType)"
            "VALUES %d, %d, %s, %d)")
      data_req = (pid, rid, reqtext, rtype)
 
      ## Insert new requirement
      sqlCursor.execute(add_req, data_req);
-     conn.close()
+     conn.commit()
      print ('Parameters for addrequirements function: %d - %d - %s - %d', pid, rid, reqtext, rtype)
      print ('query: %s%s', add_req, data_req)
      return
     ##------------------------
 
-#------------------------
+##------------------------
+def addprojectteam(pid, tid, tmname, desig):
+     "Add a new requirement to the projectteam table"
+     add_req = ("INSERT INTO projectteam "
+           "(projectID, teamMemberID, teamMemberName, designation)"
+           "VALUES %d, %d, %s, %d)")
+     data_req = (pid, rid, reqtext, rtype)
+
+     ## Insert new Team Member
+     sqlCursor.execute(add_req, data_req);
+     conn.commit()
+     print ('Parameters for addprojectteam function: %d - %d - %s - %d', pid, tid, tmname, desig)
+     print ('query: %s%s', add_req, data_req)
+     return
+    ##------------------------
+
+##------------------------
+def addprojects(pid, pname, pdesc, pown, pteam, sdate, edate):
+     "Add a new requirement to the projects table"
+     add_req = ("INSERT INTO projects "
+           "(projectID, projectName, projectDesc, projectOwner, projectTeam, startDate, endDate)"
+           "VALUES %d, %s, %s, %s, %s, %s, %s)")
+     data_req = (pid, pname, pdesc, pown, pteam, sdate, edate)
+
+     ## Insert new projects
+     sqlCursor.execute(add_req, data_req);
+     conn.commit()
+     print ('Parameters for addprojects function: %d - %s - %s - %s - %s - %s - %s', pid, pname, pdesc, pown, pteam, sdate, edate)
+     print ('query: %s%s', add_req, data_req)
+     return
+    ##------------------------
+
+##------------------------
+def addprojectrisks(pid, rid, rdesc, rpri, rstat):
+     "Add a new requirement to the projectrisks table"
+     add_req = ("INSERT INTO projectrisks "
+           "(projectID, projectName, projectDesc, projectOwner, projectTeam, startDate, endDate)"
+           "VALUES %d, %d, %s, %d, %s)")
+     data_req = (pid, rid, rdesc, rpri, rstat)
+
+     ## Insert new projectrisks
+     sqlCursor.execute(add_req, data_req);
+     conn.commit()
+     print ('Parameters for addprojectrisks function: %d - %d - %s - %d - %s', pid, rid, rdesc, rpri, rstat)
+     print ('query: %s%s', add_req, data_req)
+     return
+    ##------------------------
+
+##------------------------
+def addprojecthours(pid, rid, tid, ahr, deshr, devhr, thr, mhr):
+     "Add a new requirement to the projecthours table"
+     add_req = ("INSERT INTO projecthours "
+           "(projectID, projectName, projectDesc, projectOwner, projectTeam, startDate, endDate)"
+           "VALUES %d, %d, %s, %d, %s)")
+     data_req = (pid, rid, tid, ahr, deshr, devhr, thr, mhr)
+
+     ## Insert new projecthours
+     sqlCursor.execute(add_req, data_req);
+     conn.commit()
+     print ('Parameters for addprojecthours function: %d - %d - %d - %d - %d - %d - %d - %d', pid, rid, tid, ahr, deshr, devhr, thr, mhr)
+     print ('query: %s%s', add_req, data_req)
+     return
+    ##------------------------
+
+##------------------------
 def getrequirements(pid):
      "Retrieve the requirements for a given project ID"
 
@@ -30,7 +92,7 @@ def getrequirements(pid):
      print ('Parameters for getrequirements function: %d', pid)
      return
 
-#------------------------
+##------------------------
 def setrequirements(pid, rid, reqtext, rtype):
      "Update the requirements for a given project ID"
 
@@ -41,17 +103,49 @@ def setrequirements(pid, rid, reqtext, rtype):
      print ('query: %s', query)
      return
 
-#------------------------
+##------------------------
 def delrequirements(pid, rid):
-     "Delete the requirements for a given project ID"
-#     db_config = read_db_config()
 
-     query = "DELETE FROM requirements WHERE projectID = %d and reqID = %d"
-     sqlCursor.execute(query, (pid, rid,))
-     conn.commit()
-     print ('Parameters for delrequirements function: %d - %d', pid, rid)
-     print ('query: %s', query)
-     return
+    sqlCursor.execute("DELETE FROM requirements WHERE projectID =" +str(pid)+ " and reqID = "+str(rid))
+    conn.commit()
+    print('done')
+    return
+
+
+##------------------------
+def delprojectteam(pid, tid):
+
+    sqlCursor.execute("DELETE FROM projectteam WHERE projectID =" +str(pid)+ " and reqID = "+str(tid))
+    conn.commit()
+    print('done')
+    return
+
+
+##------------------------
+def delprojects(pid):
+
+    sqlCursor.execute("DELETE FROM projects WHERE projectID =" +str(pid))
+    conn.commit()
+    print('done')
+    return
+
+
+##------------------------
+def delprojectrisks(pid, rid):
+
+    sqlCursor.execute("DELETE FROM projectrisks WHERE projectID =" +str(pid)+ " and reqID = "+str(rid))
+    conn.commit()
+    print('done')
+    return
+
+
+##------------------------
+def delprojecthours(pid, rid, tid):
+
+    sqlCursor.execute("DELETE FROM projecthours WHERE projectID =" +str(pid)+ " and reqID = " +str(rid)+ " and teamMemberID = " +str(tid))
+    conn.commit()
+    print('done')
+    return
 
 
 
@@ -59,8 +153,8 @@ def delrequirements(pid, rid):
 
 
 ##test data
-##addrequirements( 1,1,'This is functional requirement text', 1 );
-getrequirements( 1 );
+addrequirements( 1,1,'This is functional requirement text', 1 );
+##getrequirements( 1 );
 ##setrequirements( 1,1,'This is changed functional requirement text', 1 );
-delrequirements( 1, 1 );
+#delrequirements(1,2);
 
